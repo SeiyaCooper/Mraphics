32f5657e0145458beb63072eec1ebd384bbac876
SeiyaCooper <120044030+SeiyaCooper@users.noreply.github.com>
2025-12-20 12:07:40 +0800

    chore: update `Cargo.toml`s to get ready for publishing

---
+++ packages/mraphics-core/src/constants/colors_bak.rs
@@ -0,0 +1,41 @@
+pub const WHITE: &'static str = "#ffffffff";
+pub const BLACK: &'static str = "#000000ff";
+pub const TRANSPARENT: &'static str = "#00000000";
+
+pub const RED_A: &'static str = "#e59c9cff";
+pub const RED_B: &'static str = "#ee7979ff";
+pub const RED_C: &'static str = "#f25050ff";
+pub const RED_D: &'static str = "#e73232ff";
+pub const RED_E: &'static str = "#d71e1eff";
+pub const RED: &'static str = RED_C;
+
+pub const BLUE_A: &'static str = "#5abcfbff";
+pub const BLUE_B: &'static str = "#36acf9ff";
+pub const BLUE_C: &'static str = "#159ff9ff";
+pub const BLUE_D: &'static str = "#058ae0ff";
+pub const BLUE_E: &'static str = "#127dc3ff";
+pub const BLUE: &'static str = BLUE_C;
+
+pub const YELLOW_A: &'static str = "#f7d079ff";
+pub const YELLOW_B: &'static str = "#ffc235ff";
+pub const YELLOW_C: &'static str = "#f7b722ff";
+pub const YELLOW_D: &'static str = "#e0a621ff";
+pub const YELLOW_E: &'static str = "#c58f13ff";
+pub const YELLOW: &'static str = YELLOW_C;
+
+pub const GREEN_A: &'static str = "#8bf4b7ff";
+pub const GREEN_B: &'static str = "#43f98fff";
+pub const GREEN_C: &'static str = "#0eee6bff";
+pub const GREEN_D: &'static str = "#0fd762ff";
+pub const GREEN_E: &'static str = "#04ba50ff";
+pub const GREEN: &'static str = GREEN_C;
+
+pub const GRAY_A: &'static str = "#d3d3d3ff";
+pub const GRAY_B: &'static str = "#b5b5b5ff";
+pub const GRAY_C: &'static str = "#919191ff";
+pub const GRAY_D: &'static str = "#474747ff";
+pub const GRAY_E: &'static str = "#262628ff";
+pub const GRAY: &'static str = GRAY_C;
+
+pub const COOPER_ORANGE: &'static str = "#ff936dff";
+pub const SEIYA_PINK: &'static str = "#f18eb6ff";
