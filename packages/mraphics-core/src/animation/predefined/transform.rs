eae4cf66ee753daf6284d2b9aa2d2a514c035ce7
SeiyaCooper <120044030+SeiyaCooper@users.noreply.github.com>
2026-03-15 22:53:02 +0800

    feat: add `ComplexFuncTransform`

--- packages/mraphics-core/src/animation/predefined/transform.rs
+++ packages/mraphics-core/src/animation/predefined/transform.rs
@@ -3,7 +3,7 @@ use crate::{
     Representable, Scene,
     anim_curve::{AnimCurve, Linear},
 };
-use nalgebra::{Matrix3, Vector3};
+use nalgebra::{Complex, Matrix3, Vector3};
 use std::{cell::RefCell, marker::PhantomData, rc::Rc};
 
 /// Requriements to perform a [`PointwiseTransform`] or [`MatrixTransform`]
@@ -203,6 +203,118 @@ where
                         .apply_transform(|point: &[f32; 3]| {
                             let transformed = self.matrix * &Vector3::from_row_slice(point);
                             return [transformed[0], transformed[1], transformed[2]];
+                        }),
+                );
+            }
+        });
+
+        out.on_update = Box::new({
+            let original = original.clone();
+            let transformed = transformed.clone();
+            move |p: f32, _t: f32| {
+                original
+                    .borrow()
+                    .as_ref()
+                    .unwrap()
+                    .interpolate(
+                        &transformed.borrow().as_ref().unwrap(),
+                        self.curve.sample(p),
+                    )
+                    .update_instance(
+                        &mut scene
+                            .borrow_mut()
+                            .acquire_instance_mut_unchecked(self.mesh_id),
+                    );
+            }
+        });
+
+        out.on_stop = Box::new({
+            let transformed = transformed.clone();
+            move || {
+                mesh_pool
+                    .borrow_mut()
+                    .acquire_mesh_mut_unchecked::<M>(self.mesh_id)
+                    .update_from_intermediate(transformed.borrow().as_ref().unwrap());
+            }
+        });
+
+        out
+    }
+}
+
+pub struct ComplexFuncTransform<Trans, M>
+where
+    Trans: Fn(&Complex<f32>) -> Complex<f32> + 'static,
+    M: Transformable + 'static,
+    M::Intermediate: Interpolatable + InstanceUpdater,
+{
+    /// The unique identifier of the mesh to animate.
+    pub mesh_id: MraphicsID,
+
+    pub transform: Trans,
+
+    pub curve: Box<dyn AnimCurve>,
+
+    _marker: PhantomData<M>,
+}
+
+impl<Trans, M> ComplexFuncTransform<Trans, M>
+where
+    Trans: Fn(&Complex<f32>) -> Complex<f32> + 'static,
+    M: Transformable + 'static,
+    M::Intermediate: Interpolatable + InstanceUpdater,
+{
+    pub fn new(mesh_handle: &MeshHandle<M>, transform: Trans) -> Self {
+        Self {
+            mesh_id: mesh_handle.identifier(),
+            transform,
+
+            curve: Box::new(Linear),
+
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn with_curve<Curve: AnimCurve + 'static>(mut self, curve: Curve) -> Self {
+        self.curve = Box::new(curve);
+        self
+    }
+}
+
+impl<Trans, M> Animation<'static> for ComplexFuncTransform<Trans, M>
+where
+    Trans: Fn(&Complex<f32>) -> Complex<f32> + 'static,
+    M: Transformable + 'static,
+    M::Intermediate: Interpolatable + InstanceUpdater,
+{
+    fn into_action(
+        self,
+        mesh_pool: Rc<RefCell<MeshPool>>,
+        scene: Rc<RefCell<Scene>>,
+    ) -> Action<'static> {
+        let mut out = Action::new();
+        let original = Rc::new(RefCell::new(None));
+        let transformed = Rc::new(RefCell::new(None));
+
+        out.on_start = Box::new({
+            let oringinal = original.clone();
+            let transformed = transformed.clone();
+            let mesh_pool = mesh_pool.clone();
+            move || {
+                *oringinal.borrow_mut() = Some(
+                    mesh_pool
+                        .borrow()
+                        .acquire_mesh_unchecked::<M>(self.mesh_id)
+                        .as_intermediate(),
+                );
+
+                *transformed.borrow_mut() = Some(
+                    mesh_pool
+                        .borrow()
+                        .acquire_mesh_unchecked::<M>(self.mesh_id)
+                        .apply_transform(|point: &[f32; 3]| {
+                            let transformed = (self.transform)(&Complex::new(point[0], point[1]));
+                            return [transformed.re, transformed.im, point[2]];
                         }),
                 );
             }
